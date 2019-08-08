f240d465c64f295dbcda237970170eb705e80532
liubanglong <liubanglong@smartpos.top>
2019-08-08 09:00:44 +0800

    增加零售展示大屏，一个商户不显示

--- controller/DTenant/index.js
+++ controller/DTenant/index.js
@@ -5,7 +5,9 @@ app.post("/index", function (req, res, next) {
     if (!req.session.user.is_product) {
         res.render("forbid", {});
     } else {
-        res.render("index", {});
+        res.render("index", {
+            tenantId: req.session.user.tenant_id
+        });
     }
 });
 exports.app = app;
